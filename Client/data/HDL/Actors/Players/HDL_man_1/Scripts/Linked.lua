b3fb0b0e5f367befc86b49f08f41dc203b2d0df2
johnxgithub <johnxthe9@gmail.com>
2013-08-04 14:27:34 +0800

    添加男主角的特殊技能

--- Client/data/HDL/Actors/Players/HDL_man_1/Scripts/Linked.lua
+++ Client/data/HDL/Actors/Players/HDL_man_1/Scripts/Linked.lua
@@ -1145,6 +1145,42 @@ skills[200020] =
 		return true;
 	end,
 }
+skills[200030] = 
+{
+	name = "特效技能",
+	Actions = 
+	{
+		["A"] =
+		{
+			name = "sepcialskill",
+			loop = false,
+			stance = "ground_action",
+			weapon = 0,
+			switch = 1,
+			priority = 0,
+			nextAction = "Idle",
+			
+			IsReady = function(this)
+				return true;
+			end,
+			
+			Events = 
+			{
+				["Shoot1"] = 
+				{
+					time = 0.01,
+					Func = function(this)
+						Self:PlayGlobalEffect{id = 200030};
+					end,
+				},	
+			},
+		},	
+	},
+	
+	IsReady = function()
+		return true;
+	end,
+}
 skills[200011] = 
 {
 	name = "第一把枪",
---
+++ Client/data/HDL/Actors/Players/HDL_man_1/Scripts/Skill/200030.lua
@@ -0,0 +1,36 @@
+skills[200030] = 
+{
+	name = "特效技能",
+	Actions = 
+	{
+		["A"] =
+		{
+			name = "sepcialskill",
+			loop = false,
+			stance = "ground_action",
+			weapon = 0,
+			switch = 1,
+			priority = 0,
+			nextAction = "Idle",
+			
+			IsReady = function(this)
+				return true;
+			end,
+			
+			Events = 
+			{
+				["Shoot1"] = 
+				{
+					time = 0.01,
+					Func = function(this)
+						Self:PlayGlobalEffect{id = 200030};
+					end,
+				},	
+			},
+		},	
+	},
+	
+	IsReady = function()
+		return true;
+	end,
+}
